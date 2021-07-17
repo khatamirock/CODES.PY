793e45149c542f004c6b4339c7a79c5d04034e13
ucheckit7@gmail.com <777788667555887777>
2021-07-17 10:58:05 +0600

    new Commiter___17.07.21__10:45AM

--- recursion/burgler_noghtOut.py
+++ recursion/burgler_noghtOut.py
@@ -1,10 +1,19 @@
 
 
-
+ar=[3,8,10,4,1,7]
 def brgl(n):
 
-    for x in range(n):
-        pass
+    for x in range(2,n):
+        if x==2:
+          ar[x]+=ar[x-2]
+        else:
+            ar[x]+=max(ar[x-2],ar[x-3])
+
+    return max(ar[-2:])
+
+
+print(  brgl(len(ar))  )
+
 
 
 
